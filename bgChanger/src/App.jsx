fae65599fef57f36e72edebce83911cb51c368d8
Ayushman404 <ayushmankumar790333@gmail.com>
2025-05-21 17:02:59 +0530

    minor changes

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -8,12 +8,12 @@ function App() {
       <div className='w-[100vw] h-screen duration-200' style = {{backgroundColor: color}}>
         <div className='flex flex-wrap fixed justify-center items-center bottom-12 inset-x-0 px-2'>
           <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-2 py-3 rounded-xl">
-            <button className='bg-red-500' onClick={()=>setColor("red")}>Red</button>
-            <button className='bg-blue-500' onClick={()=>setColor("blue")}>Blue</button>
-            <button className="bg-yellow-500" onClick={()=>setColor("yellow")}>Yellow</button>
-            <button className="bg-emerald-500" onClick={()=>setColor("#50C878")}>Emerald</button>
-            <button className="bg-purple-400" onClick={()=>setColor("#E6E6FA")}>Lavender</button>
-            <button className='bg-red-300' onClick={()=>setColor("salmon")}>Salmon</button>
+            <button className='bg-red-500 rounded-xl px-3 py-2 text-white hover:brightness-110 cursor-pointer' onClick={()=>setColor("red")}>Red</button>
+            <button className='bg-blue-500 rounded-xl px-3 py-2 text-white hover:brightness-110 cursor-pointer' onClick={()=>setColor("blue")}>Blue</button>
+            <button className="bg-yellow-500 rounded-xl px-3 py-2 text-white hover:brightness-110 cursor-pointer" onClick={()=>setColor("yellow")}>Yellow</button>
+            <button className="bg-emerald-500 rounded-xl px-3 py-2 text-white hover:brightness-110 cursor-pointer" onClick={()=>setColor("#50C878")}>Emerald</button>
+            <button className="bg-purple-400 rounded-xl px-3 py-2 text-white hover:brightness-110 cursor-pointer" onClick={()=>setColor("#E6E6FA")}>Lavender</button>
+            <button className='bg-red-300 rounded-xl px-3 py-2 text-white hover:brightness-110 cursor-pointer' onClick={()=>setColor("salmon")}>Salmon</button>
           </div>
         </div>
       </div>
